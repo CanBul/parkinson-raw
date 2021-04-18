48de5723faf037001a16e5ec06ac3cd407b1e43e
Can Bulguoglu <canbulguoglu@msn.com>
2021-04-18 17:05:21 +0300

    fixed shape error

--- model.py
+++ model.py
@@ -9,12 +9,6 @@ from tensorflow import keras
 from augmentation import Augmentation
 import os
 
-
-
-    
-
-
-
 def create_batch(directories, test=False):
 
     x_data= []
@@ -35,39 +29,32 @@ def create_batch(directories, test=False):
     
     return np.array(x_data), np.array(y_data)
 
+def create_model():
+
+    model = Sequential()
+
+    model.add(Convolution2D(filters = 32, kernel_size = (5,5),padding = 'Same',
+                    activation ='relu', input_shape = (28,28,1)))
+    model.add(Convolution2D(filters = 32, kernel_size = (5,5),padding = 'Same',
+                    activation ='relu'))
+    model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(Dropout(0.25))
 
 
-model = Sequential()
-
-model.add(Convolution2D(filters = 32, kernel_size = (5,5),padding = 'Same',
-                 activation ='relu', input_shape = (28,28,1)))
-model.add(Convolution2D(filters = 32, kernel_size = (5,5),padding = 'Same',
-                 activation ='relu'))
-model.add(MaxPooling2D(pool_size=(2,2)))
-model.add(Dropout(0.25))
+    model.add(Convolution2D(filters = 64, kernel_size = (3,3),padding = 'Same',
+                    activation ='relu'))
+    model.add(Convolution2D(filters = 64, kernel_size = (3,3),padding = 'Same',
+                    activation ='relu'))
+    model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
+    model.add(Dropout(0.25))
 
 
-model.add(Convolution2D(filters = 64, kernel_size = (3,3),padding = 'Same',
-                 activation ='relu'))
-model.add(Convolution2D(filters = 64, kernel_size = (3,3),padding = 'Same',
-                 activation ='relu'))
-model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
-model.add(Dropout(0.25))
+    model.add(Flatten())
+    model.add(Dense(256, activation = "relu"))
+    model.add(Dropout(0.5))
+    model.add(Dense(1, activation = "sigmoid"))
 
-
-model.add(Flatten())
-model.add(Dense(256, activation = "relu"))
-model.add(Dropout(0.5))
-model.add(Dense(1, activation = "sigmoid"))
-
-optimizer = RMSprop(lr=0.001, rho=0.9, epsilon=1e-08, decay=0.0)
-model.compile(optimizer = optimizer , loss='binary_crossentropy', metrics=['accuracy'])
-
-earlyStopping = EarlyStopping(monitor='val_acc', patience=30, verbose=0, mode='max')
-mcp_save = ModelCheckpoint('mdl_wts.hdf5', save_best_only=True, monitor='val_acc', mode='max')
-reduce_lr_loss = ReduceLROnPlateau(monitor='val_acc', factor=0.5, patience=3, verbose=1, min_lr=0.00001)
-batch_size =128
-print('Training has started...')
+    return model
 
 
 
@@ -78,12 +65,30 @@ for each in range(len(paths)):
     train_path = [path for path in paths if path != test_path[0]]
 
     x_test, y_test = create_batch(test_path, test=True)
+    x_test = x_test.reshape(-1,128,130,1)
 
+    
+
+    model = create_model()
+
+    optimizer = RMSprop(lr=0.001, rho=0.9, epsilon=1e-08, decay=0.0)
+    model.compile(optimizer = optimizer , loss='binary_crossentropy', metrics=['accuracy'])
+
+    earlyStopping = EarlyStopping(monitor='val_acc', patience=30, verbose=0, mode='max')
+    mcp_save = ModelCheckpoint('test_fold_model' + str(each).hdf5', save_best_only=True, monitor='val_acc', mode='max')
+    reduce_lr_loss = ReduceLROnPlateau(monitor='val_acc', factor=0.5, patience=3, verbose=1, min_lr=0.00001)
+
+
+    batch_size =128
     epochs = 1
+    
 
     for epoch in range(epochs):
         x_train, y_train = create_batch(train_path)
+        x_train = x_train.reshape(-1,128,130,1)
 
+        
+        print('Training has started...')
         history = model.fit(x_train,y_train, batch_size=batch_size, verbose=2,
                                 epochs = 1, validation_data = (x_test,y_test))#, steps_per_epoch=x_train.shape[0] // batch_size, callbacks=[earlyStopping, mcp_save, reduce_lr_loss])
 
